8f5a14c1110c690460c3fd164b2b31b18c32789d
Aleksa Stojkovic <alexstoj.ns@gmail.com>
2021-04-15 18:28:03 +0200

    Dodat je prvi zadatak za 5-ti domaci

---
+++ Domaci5_1_A.c
@@ -0,0 +1,27 @@
+#include <bcm2835.h>
+#include <stdio.h>
+#include <unistd.h>
+#define MAX_VAL 25
+
+unsigned char val = 0;
+	
+int main(void)
+{
+	if(!bcm2835_init())
+		return 1;
+	
+	bcm2835_i2c_begin();
+	bcm2835_i2c_setSlaveAddress(0x48);
+	bcm2835_i2c_set_baudrate(10000);
+	while(1)
+	{
+		val = (val + 1) % MAX_VAL;
+		bcm2835_i2c_write((const char *)&b, 2);
+		bcm2835_delay(50);
+	}
+	
+	bcm2835_i2c_end();
+	bcm2835_close();
+	
+	return 0;
+}
