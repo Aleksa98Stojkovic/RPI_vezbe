3d3bae138e6cd69c4df0128e2794f2419c659b38
Aleksa Stojkovic <alexstoj.ns@gmail.com>
2021-04-26 22:43:19 +0200

    Samo da prebacim na Linux C++ model

---
+++ EDSR_CPP/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+typedef double t;
+typedef std :: vector < std :: vector < std :: vector <t>>> float3D;
+typedef std :: vector < std :: vector < std :: vector < std :: vector <t>>>> float4D;
+
+#endif // COMMON_H_INCLUDED
